74baf6fd9447405cc5e27696206467dab49ab366
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-22 17:47:20 +0300

    added login page to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
+import Header from "@/components/Header";
+import Login from "@/components/Login";
+import { useAddress } from "@thirdweb-dev/react";
 import { NextPage } from "next";
+import Head from "next/head";
 
-export const Home: NextPage = () => {
-  return <></>;
+const Home: NextPage = () => {
+  const address = useAddress();
+  if (!address) {
+    return <Login />;
+  }
+
+  return (
+    <div className="bg-[#091b18] min-h-screen flex flex-col">
+      <Head>
+        <title></title>
+      </Head>
+      <Header />
+    </div>
+  );
 };
+export default Home;
