f85ac4841fc12045964fca34e9d768f076ce5659
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-22 18:11:24 +0300

    added loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="bg-[#091b18] min-h-screen flex flex-col items-center justify-center text-center">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl text white">Loading...</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
