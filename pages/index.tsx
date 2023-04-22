65e5d8190d99c1803ffb1d21a1dd9386b42cd039
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-22 18:10:50 +0300

    connected smart contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import Header from "@/components/Header";
+import Loading from "@/components/Loading";
 import Login from "@/components/Login";
-import { useAddress } from "@thirdweb-dev/react";
+import { useAddress, useContract } from "@thirdweb-dev/react";
 import { NextPage } from "next";
 import Head from "next/head";
 
+const CONTRACT_ADDRESS =
+  process.env.NEXT_PUBLICK_LOTTERY_CONTRACT_ADDRES ||
+  "0xaA65c64a0c17477156B6513AbF2bC57ACC503524";
 const Home: NextPage = () => {
   const address = useAddress();
+  const { contract, isLoading } = useContract(CONTRACT_ADDRESS);
+
   if (!address) {
     return <Login />;
+  }
+
+  if (isLoading) {
+    return <Loading />;
   }
 
   return (
