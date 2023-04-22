117551ca1631bfcbe8b4a1be7fdba63c12fa405a
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-22 20:38:50 +0300

    added contract hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import CountdownTimer from "@/components/CountdownTimer";
 import Header from "@/components/Header";
 import Loading from "@/components/Loading";
 import Login from "@/components/Login";
 import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import { ethers } from "ethers";
 import { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
@@ -9,6 +11,14 @@ import { useState } from "react";
 const CONTRACT_ADDRESS =
   process.env.NEXT_PUBLICK_LOTTERY_CONTRACT_ADDRES ||
   "0xaA65c64a0c17477156B6513AbF2bC57ACC503524";
+
+const convertToEtherPrice = (price: number): number =>
+  price && Number(ethers.utils.formatEther(price.toString()));
+
+const CURRENCY = "MATIC";
+const priceView = (price: number) =>
+  `${convertToEtherPrice(price)} ${CURRENCY}`;
+
 const Home: NextPage = () => {
   const address = useAddress();
   const { contract, isLoading } = useContract(CONTRACT_ADDRESS);
@@ -16,8 +26,26 @@ const Home: NextPage = () => {
     contract,
     "RemainingTickets"
   );
-  console.log("data", remainingTickets);
+
+  const { data: currentWinningReward } = useContractRead(
+    contract,
+    "CurrentWinningReward"
+  );
+  const { data: ticketCommission } = useContractRead(
+    contract,
+    "ticketCommission"
+  );
+
+  const { data: expiration, isLoading: isLoadingExpiration } = useContractRead(
+    contract,
+    "expiration"
+  );
+
+  const { data: ticketPrice } = useContractRead(contract, "ticketPrice");
+
   const [quantity, setQuantity] = useState<number>(1);
+  const isTicketsExpired = expiration?.toString() < Date.now().toString();
+
   if (!address) {
     return <Login />;
   }
@@ -29,13 +57,12 @@ const Home: NextPage = () => {
   return (
     <div className="bg-[#091b18] min-h-screen flex flex-col">
       <Head>
-        <title></title>
+        <title>Lottery</title>
       </Head>
       <div className="flex-1">
         <Header />
 
         <div className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5">
-          {/* Draw container */}
           <div className="stats-container">
             <h1 className="text-5xl text-white font-semibold text-center">
               The Next Draw
@@ -43,22 +70,26 @@ const Home: NextPage = () => {
             <div className="flex justify-between p-2 space-x-2">
               <div className="stats">
                 <h2 className="text-sm">Total Pool</h2>
-                <p className="text-xl">0.1 MATIC</p>
+                <p className="text-xl">{priceView(currentWinningReward)}</p>
               </div>
               <div className="stats">
                 <h2 className="text-sm">Tickets Remaining</h2>
                 <p className="text-xl">{remainingTickets?.toNumber()}</p>
               </div>
             </div>
+            <div className="mt-5 mb-3">
+              <CountdownTimer
+                expiration={expiration}
+                isLoadingExpiration={isLoadingExpiration}
+              />
+            </div>
           </div>
-
-          {/* Stats */}
 
           <div className="stats-container space-y-2">
             <div className="stats-container">
               <div className="flex justify-between items-center text-white pb-2">
                 <h2>Price per ticket</h2>
-                <p>0.01 MATIC</p>
+                <p>{priceView(ticketPrice)}</p>
               </div>
               <div className="flex text-white items-center space-x-2 bg-[#091b18] border-[#004337] border p-4">
                 <p>TICKETS</p>
@@ -75,11 +106,11 @@ const Home: NextPage = () => {
               <div className="space-y-2 mt-5">
                 <div className="flex items-center justify-between text-emerald-300 text-xs italic font-extrabold">
                   <p>Total cost of tickets</p>
-                  <p>0.999</p>
+                  <p>{convertToEtherPrice(ticketPrice) * quantity}</p>
                 </div>
                 <div className="flex items-center justify-between text-emerald-300 text-xs italic">
                   <p>Service fees</p>
-                  <p>0.001 MATIC</p>
+                  <p>{priceView(ticketCommission)}</p>
                 </div>
                 <div className="flex items-center justify-between text-emerald-300 text-xs italic">
                   <p>+ Network Fees</p>
@@ -87,7 +118,9 @@ const Home: NextPage = () => {
                 </div>
               </div>
               <button
-                disabled={false}
+                disabled={
+                  isTicketsExpired || remainingTickets?.toNumber() === 0
+                }
                 className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md 
               text-white shadow-xl disabled:from-gray-600 disabled:to-gray-100 
               disabled:cursor-not-allowed disabled:text-gray-100"
