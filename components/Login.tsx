8b3c6fa00ca776222bbaa2d5af0bc21854bf8810
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-22 17:46:41 +0300

    added login page

---
+++ components/Login.tsx
@@ -0,0 +1,23 @@
+import { useMetamask } from "@thirdweb-dev/react";
+
+const Login = () => {
+  const connectWithMetamask = useMetamask();
+  return (
+    <div className="bg-[#091b18] min-h-screen flex flex-col items-center justify-center text-center">
+      <div className="flex flex-col items-center mb-10">
+        <h1 className="text-6xl text white fon-bold mb-5">Lottery Web3</h1>
+        <h2 className="text-white">
+          Get Started By loggin in with your Metamask
+        </h2>
+      </div>
+      <button
+        onClick={() => connectWithMetamask()}
+        className="bg-white text-[#091b18] px-8 py-5 mt-10 rounded-lg shadow-lg font-bold"
+      >
+        Loggin with Metamask
+      </button>
+    </div>
+  );
+};
+
+export default Login;
